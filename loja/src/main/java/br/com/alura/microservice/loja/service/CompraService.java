2f6bf0ff63b94e3868eea3b500cc236230ff556a
Lucas Machado <lucas.machado290@gmail.com>
2020-11-11 21:09:31 -0300

    Configuração do papertrail para rastreabilidade dos logs

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,5 +1,7 @@
 package br.com.alura.microservice.loja.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +14,18 @@ import br.com.alura.microservice.loja.model.Compra;
 @Service
 public class CompraService {
 
+	private static final Logger LOG = LoggerFactory.getLogger(CompraService.class);
+	
 	@Autowired
 	private FornecedorClient fornecedorClient;
 	
 	public Compra realizaCompra(CompraDTO compra) {
-		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
+		final String estado = compra.getEndereco().getEstado();
+		
+		LOG.info("buscando informações do fornecedor {}", estado);
+		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(estado);
+		
+		LOG.info("realizando um pedido");
 		InfoPedidoDTO pedido = fornecedorClient.realizaCompra(compra.getItens());
 		
 		Compra compraSalva = new Compra();
